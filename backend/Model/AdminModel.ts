febf267ba8faec9ffe3588706d9c04097801918d
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2023-02-08 13:47:00 +0200

    fix: Change the fields of the admin to required

--- backend/Model/AdminModel.ts
+++ backend/Model/AdminModel.ts
@@ -2,9 +2,9 @@ import mongoose, { Schema } from "mongoose";
 
 const AdminSchema = new Schema({
     username: { type: String, unique: true },
-    firstName: String,
-    lastName: String,
-    password: String,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    password: { type: String, required: true },
 });
 
 AdminSchema.index({ username: 1 }, { unique: true });
